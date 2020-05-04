d2cb4f2208e5895c654f7c2fae7e656ede2e5704
mataotao <mttwind@foxmail.com>
2020-05-04 20:38:38 +0800

    设置代理

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
+// import jsonp from 'jsonp'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  mounted() {
+    let url = '/api/common/adver-getadver'
+    axios.get(url).then((res) => {
+      console.log(res)
+    })
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
